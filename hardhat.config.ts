3d1145d1fb827b895e0811d968555e37bb58dc4c
Matteo Celani <matteocelani4@gmail.com>
2023-04-20 17:03:36 +0200

    Sepholia integration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,93 +13,111 @@ const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   etherscan: {
     apiKey: {
-      // Ethereum 
-      mainnet: process.env.ETHERSCAN_API_KEY || '',
-      goerli: process.env.ETHERSCAN_API_KEY || '',
+      // Ethereum
+      mainnet: process.env.ETHERSCAN_API_KEY || "",
+      goerli: process.env.ETHERSCAN_API_KEY || "",
       // Arbitrum
-      arbitrumOne: process.env.ARBISCAN_API_KEY || '',
-      arbitrumGoerli: process.env.ARBISCAN_API_KEY || '',
+      arbitrumOne: process.env.ARBISCAN_API_KEY || "",
+      arbitrumGoerli: process.env.ARBISCAN_API_KEY || "",
       // Avalanche
-      avalanche: process.env.SNOWTRACE_API_KEY || '',
-      avalancheFuji: process.env.SNOWTRACE_API_KEY || '',
+      avalanche: process.env.SNOWTRACE_API_KEY || "",
+      avalancheFuji: process.env.SNOWTRACE_API_KEY || "",
       // BNB Smart Chain
-      bsc: process.env.BSCSCAN_API_KEY || '',
-      bscTestnet: process.env.BSCSCAN_API_KEY || '',
+      bsc: process.env.BSCSCAN_API_KEY || "",
+      bscTestnet: process.env.BSCSCAN_API_KEY || "",
       // Polygon
-      polygon: process.env.POLYGONSCAN_API_KEY || '',
-      polygonMumbai: process.env.POLYGONSCAN_API_KEY || '',
-    }
+      polygon: process.env.POLYGONSCAN_API_KEY || "",
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY || "",
+    },
   },
   networks: {
     hardhat: {},
     local: {
-      url: 'http://127.0.0.1:8545',
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      url: "http://127.0.0.1:8545",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     // Ethereum
     mainnet: {
-      url: 'https://mainnet.infura.io/v3/' + process.env.INFURA_API_KEY || '',
+      url: "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY || "",
       gasPrice: 75000000000,
       chainId: 1,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     goerli: {
-      url: 'https://goerli.infura.io/v3/' + process.env.INFURA_API_KEY || '',
+      url: "https://goerli.infura.io/v3/" + process.env.INFURA_API_KEY || "",
       gasPrice: 75000000000,
       chainId: 5,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    sepholia: {
+      url: "https://sepolia.infura.io/v3/" + process.env.INFURA_API_KEY || "",
+      gasPrice: 75000000000,
+      chainId: 11155111,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     // Arbitrum
     arbitrumOne: {
-      url: 'https://arb1.arbitrum.io/rpc',
+      url: "https://arb1.arbitrum.io/rpc",
       gasPrice: 75000000000,
       chainId: 42161,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     arbitrumGoerli: {
-      url: 'https://goerli-rollup.arbitrum.io/rpc',
+      url: "https://goerli-rollup.arbitrum.io/rpc",
       gasPrice: 75000000000,
       chainId: 421613,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     // Avalanche
     avalanche: {
-      url: 'https://api.avax.network/ext/bc/C/rpc',
+      url: "https://api.avax.network/ext/bc/C/rpc",
       gasPrice: 225000000000,
       chainId: 43114,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     avalancheFuji: {
-      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
       gasPrice: 225000000000,
       chainId: 43113,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     // BNB Smart Chain
     bsc: {
-      url: 'https://bsc-dataseed.binance.org/',
+      url: "https://bsc-dataseed.binance.org/",
       gasPrice: 20000000000,
       chainId: 56,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     bscTestnet: {
-      url: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
       gasPrice: 20000000000,
       chainId: 97,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     // Polygon
     polygon: {
       url: "https://polygon-rpc.com/",
       chainId: 137,
       gasPrice: 8000000000,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     polygonMumbai: {
       url: "https://matic-mumbai.chainstacklabs.com",
       chainId: 80001,
       gasPrice: 8000000000,
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
   solidity: {
@@ -117,14 +135,14 @@ const config: HardhatUserConfig = {
   paths: {
     sources: "./contracts",
     cache: "./cache",
-    artifacts: "./artifacts"
+    artifacts: "./artifacts",
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
     excludeContracts: [],
     src: "./contracts",
-  }
-}
+  },
+};
 
 export default config;
