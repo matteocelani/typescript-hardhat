90a18b19ffe65a2826d11b6949413a4642604ca5
Matteo Celani <26189482+matteocelani@users.noreply.github.com>
2024-03-06 18:11:27 +0100

    WETH Mock

---
+++ contracts/mocks/WETH.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+/**
+ * @title WETH
+ * @dev Implementation of the WETH (Wrapped Ether), an ERC20-like token.
+ */
+contract WETH {
+    string public constant name = "Wrapped Ether";
+    string public constant symbol = "WETH";
+    uint8 public constant decimals = 18;
+
+    mapping(address => uint256) private balances;
+    mapping(address => mapping(address => uint256)) private allowances;
+    uint256 private totalSupply;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+
+    function getTotalSupply() public view returns (uint256) {
+        return totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        require(
+            balances[msg.sender] >= amount,
+            "WETH: transfer amount exceeds balance"
+        );
+        balances[msg.sender] -= amount;
+        balances[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool) {
+        allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public returns (bool) {
+        require(
+            amount <= balances[sender],
+            "WETH: transfer amount exceeds balance"
+        );
+        require(
+            amount <= allowances[sender][msg.sender],
+            "WETH: transfer amount exceeds allowance"
+        );
+        balances[sender] -= amount;
+        balances[recipient] += amount;
+        allowances[sender][msg.sender] -= amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function allowance(
+        address owner,
+        address spender
+    ) public view returns (uint256) {
+        return allowances[owner][spender];
+    }
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+        totalSupply += msg.value;
+        emit Transfer(address(0), msg.sender, msg.value);
+    }
+
+    function withdraw(uint256 amount) public {
+        require(balances[msg.sender] >= amount, "WETH: insufficient balance");
+        balances[msg.sender] -= amount;
+        totalSupply -= amount;
+        payable(msg.sender).transfer(amount);
+        emit Transfer(msg.sender, address(0), amount);
+    }
+
+    receive() external payable {
+        deposit();
+    }
+}
